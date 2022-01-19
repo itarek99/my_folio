8c2a0b7acb0329b64a1c081f7c51f530695bfe59
itarek99 <tarekulislam7060@gmail.com>
2022-01-19 22:06:13 +0600

    loader added

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const navList = document.querySelector('.nav-list');
 const moreBtn = document.querySelector('.more-btn');
 const footer = document.querySelector('footer');
 
+// smooth scrolling on nav-item click
 navList.addEventListener('click', function (e) {
     e.preventDefault();
     if (e.target.parentElement.classList.contains('nav-link')) {
@@ -12,16 +13,19 @@ navList.addEventListener('click', function (e) {
     }
 });
 
+// more about me button action
 moreBtn.addEventListener('click', function (e) {
     e.preventDefault();
     document.querySelector('#about').scrollIntoView({ behavior: 'smooth' });
 });
 
+// Intersection observer options
 const options = {
     root: null,
     threshold: 0.1,
 };
 
+// Intersection observer function
 const activeNav = function (entries) {
     const [entry] = entries;
     if (entry.isIntersecting) {
@@ -36,7 +40,13 @@ const activeNav = function (entries) {
     }
 };
 
+// Intersection observer
 const observer = new IntersectionObserver(activeNav, options);
 sections.forEach((section) => {
     observer.observe(section);
+});
+
+// remove loader after everything load
+window.addEventListener('load', function () {
+    document.querySelector('.loader__box').remove();
 });
